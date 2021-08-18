1067ea514e6ba94043ad46836be71e3d6778f381
TrevorZylks <trevorzylks@gmail.com>
2021-08-17 21:22:29 -0700

    done

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
   
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "Steve Urkel always says: #{self.catchphrase}"
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def actors_list
+    self.actors.map do |ha|
+        ha.first_name + " " + ha.last_name
+    end
+  end
+
+  
+
 end
